d2991d61a23fbc8bd4a9eb92b110aef1bfe884f1
Eric Dango <joegarcia11.jg@gmail.com>
2024-03-21 15:39:46 +0800

    some changes on js

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ let answer = parseInt(
   prompt("Please enter the number you would like to FizzBuzz up to: ")
 );
 
+for (let i = 0; i < answer; i++) {
+  if (i == 0) {
+    continue;
+  }
+  document.getElementById("answer").innerHTML =
+    document.getElementById("answer").textContent + " " + i;
+}
 document.getElementById("answer").innerHTML =
-  document.getElementById("answer").textContent + " " + answer;
+  document.getElementById("answer").textContent + ". <br />";
+let names = ["Roy", "Eric", "Michael"];
+for (let i = 0; i < names.length; i++) {
+  if (names[i] == "Eric") {
+    continue;
+  } else {
+    document.getElementById("answer").innerHTML =
+      document.getElementById("answer").textContent + " " + names[i];
+  }
+}
