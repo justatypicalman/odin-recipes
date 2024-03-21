08854cc91f5ae2d6e263728a41fbbfcce17118e9
Eric Dango <joegarcia11.jg@gmail.com>
2024-03-21 15:23:54 +0800

    added a js file for testing

---
+++ index.js
@@ -0,0 +1,6 @@
+let answer = parseInt(
+  prompt("Please enter the number you would like to FizzBuzz up to: ")
+);
+
+document.getElementById("answer").innerHTML =
+  document.getElementById("answer").textContent + " " + answer;
