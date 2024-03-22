2c1813a96344f87df31a458d9a3f84cf8c13a5f7
Eric Dango <joegarcia11.jg@gmail.com>
2024-03-22 11:24:28 +0800

    added nutrition facts page for practice

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ for (let i = 0; i < answer; i++) {
 }
 document.getElementById("answer").innerHTML =
   document.getElementById("answer").textContent + ". <br />";
+
+const number1 = 5;
+const number2 = 10;
+
+function add(a, b) {
+  const c = a + b;
+  console.log(c);
+  return c;
+}
+
+function print() {
+  return add(number1, number2);
+}
+
 let names = ["Roy", "Eric", "Michael"];
 for (let i = 0; i < names.length; i++) {
   if (names[i] == "Eric") {
     continue;
   } else {
-    document.getElementById("answer").innerHTML =
-      document.getElementById("answer").textContent + " " + names[i];
+    document.getElementById("answer").innerHTML +=
+      " " + names[i] + " " + print();
   }
 }
